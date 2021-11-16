490cec7ef6a1a666a29cba42e11e36b32400b975
Arlington Albertson <arlington@rebelliondefense.com>
2021-11-16 09:50:33 -0500

    initial commit

---
+++ setup.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -Eeuo pipefail
+IFS=$'\n\t'
+
+if [ "$(id -u)" -eq 0 ]; then
+    echo "🙀 This script should not be run as root!"
+    echo ""
+    echo "Please run this as your user, you will be prompted for your password when necessary."
+    exit 1
+fi
+
+if [ -f xcode-select -p 1>/dev/null;echo $? ]; then
+  echo "XCode Command Line Tools are installed. Continuing..."
+else
+  echo "Installing the Command Line Tools (expect a GUI popup)"
+  sudo /usr/bin/xcode-select --install
+  echo "Relaunch this script when the installation has completed."
+  exit 1
+fi
+
+
+exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if ! exists brew; then
+  echo "this script requires 'homebrew'."
+  echo "installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew doctor
+else
+  echo "homebrew is installed, continuing..."
+fi
+
+if ! exists python3; then
+  echo "this script requires 'python3'."
+  echo "installing..."
+  export PATH="/usr/local/opt/python/libexec/bin:$PATH"
+  brew install python
+  sudo pip --upgrade pip
+else
+  echo "python3 is installed, continuing..."
+fi
+
+if ! exists ansible; then
+  echo "this script requires 'ansible'."
+  echo "installing..."
+  sudo pip3 install ansible
+  sudo pip3 install --upgrade ansible
+else
+  echo "ansible is installed, continuing..."
+fi
+
+if ansible-playbook -i hosts.ini playbook.yml; then
+# if ansible-playbook -i hosts.ini playbook.yml --ask-become-pass; then
+    echo ""
+    echo "    Laptop setup complete!"
+    echo ""
+    osascript -e 'display notification "Your laptop should be ready to use now! 😸" with title "Laptop Setup Complete"'
+else
+    echo ""
+    echo "    Laptop setup Failed :("
+    echo ""
+    echo "    It is safe to re-run this script as many times as you"
+    echo "    need to."
+    echo ""
+    osascript -e 'display notification "Automated laptop setup has failed! 🙀" with title "Laptop Setup Failed!"'
+fi
